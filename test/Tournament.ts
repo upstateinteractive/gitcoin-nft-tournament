4740d679dd714636b9efe7f95932a64079ea9e8e
Lorran Sutter <sutterlorran@gmail.com>
2021-05-21 15:37:14 -0400

    fix: PR fixes

--- test/Tournament.ts
+++ test/Tournament.ts
@@ -112,12 +112,18 @@ describe("Tournament", function () {
       )
         .to.be.revertedWith("Tournament must not be reached the last round")
     })
+    it("should emit RoundEnded event", async function () {
+      await expect(
+        tournament.connect(owner).endCurrentRound(1, C.roundResults1)
+      )
+        .to.emit(tournament, "RoundEnded").withArgs(1, 3)
+    })
     it("should correctly update balances", async () => {
       await tournament.connect(owner).endCurrentRound(1, C.roundResults1)
       const balancesAfterRound1 = await tournament.getCurrentBalances(1)
 
       balancesAfterRound1.forEach((balance, i) => expect(balance).to.be.equal(ethers.BigNumber.from(C.balancesAfterRound1[i])))
-      
+
       await tournament.connect(owner).endCurrentRound(1, C.roundResults2)
       const balancesAfterRound2 = await tournament.getCurrentBalances(1)
 
@@ -128,7 +134,7 @@ describe("Tournament", function () {
       const bracketWinnersAfterRound1 = await tournament.getBracketWinners(1)
 
       bracketWinnersAfterRound1.forEach((winner, i) => expect(winner).to.be.equal(ethers.BigNumber.from(C.bracketWinnersAfterRound1[i])))
-      
+
       await tournament.connect(owner).endCurrentRound(1, C.roundResults2)
       const bracketWinnersAfterRound2 = await tournament.getBracketWinners(1)
 
@@ -166,6 +172,14 @@ describe("Tournament", function () {
         tournament.connect(owner).endTournament(1, C.roundResults3)
       )
         .to.be.revertedWith("Tournament must be in the last round")
+    })
+    it("should emit TournamentEnded event", async function () {
+      await tournament.connect(owner).endCurrentRound(1, C.roundResults1)
+      await tournament.connect(owner).endCurrentRound(1, C.roundResults2)
+      await expect(
+        tournament.connect(owner).endTournament(1, C.roundResults3)
+      )
+        .to.emit(tournament, "TournamentEnded").withArgs(1)
     })
     it("should correctly update balances", async () => {
       await tournament.connect(owner).endCurrentRound(1, C.roundResults1)
